0dd4dbada78d53091ef25341609e6773315c93e8
mrdeveloper917 <thakurayush995537@gmail.com>
2026-02-09 13:12:25 +0530

    connect db

--- server/server.js
+++ server/server.js
@@ -2,12 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
-const authRoutes = require("./routes/authRoutes"); 
+const authRoutes = require("./routes/authRoutes");
+const mongoose = require("mongoose");
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected Successfully");
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB Connection Error:", err);
+  });
 
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
